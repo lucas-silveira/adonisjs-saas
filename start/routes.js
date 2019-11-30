a311ad3398b7a69d31907a2b03a54b9cdad2f688
Lucas Silveira <lugano1995@gmail.com>
2019-11-29 21:19:39 -0300

    created routes members and permissions

--- start/routes.js
+++ start/routes.js
@@ -38,4 +38,7 @@ Route.group(() => {
       ]
     )
   )
+  Route.get('members', 'MemberController.index')
+  Route.put('members/:id', 'MemberController.update').middleware('is:administrator')
+  Route.get('permissions', 'PermissionController.show')
 }).middleware(['auth', 'team'])
